4a3a2be5d5b9f5ce66b0687af120a1f7f4f592f0
dancingkimDH <kimdonghyun0701@gmail.com>
2023-06-30 12:29:48 +0900

    first commit

---
+++ src/main/java/com/example/study_springboots_donghyun/StudySpringbootsDonghyunApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_springboots_donghyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringbootsDonghyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringbootsDonghyunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/study_springboots_donghyun/StudySpringbootsDonghyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_springboots_donghyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringbootsDonghyunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
